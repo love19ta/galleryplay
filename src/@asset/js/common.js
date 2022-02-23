01e9b7c2c1fcfd510652997e9ab7398548d2e2da
love19ta <46180204+love19ta@users.noreply.github.com>
2022-02-23 16:18:57 +0900

    Add files via upload

---
+++ src/@asset/js/common.js
@@ -0,0 +1,95 @@
+const base_url = (location.pathname.indexOf('build') > 0) ? location.pathname.split('build')[0]+'build' : '';
+const isMobile = (window.innerWidth > 1024) ? false : true;
+
+const modals = (function(){
+    let $wrap = $('#wrap');
+    let $modals = $('<div class="modals" />');
+
+    const open = function(el){
+        if($modals == null) $modals = $('<div class="modals" />');
+        
+        let modalCode = $(el).data('modal');
+        
+        $wrap.append($modals);
+        $modals.load( base_url + '/modals/'+ modalCode +'.html #modal_'+ modalCode, function(){
+            $modals.addClass('show');
+            setTimeout(function(){
+                let $btnClose = $modals.find('.modal_close');
+                $btnClose.focus();
+            }, 500);
+        });
+
+    }
+    const close = function(){
+        $modals.removeClass('show');
+        setTimeout(function(){
+            $modals.remove();
+            $modals = null;
+        },500);
+    }
+
+    return {
+        open: open,
+        close: close
+    }
+})();
+
+const userMenu = function(){
+    let $headerNav = $('.header__nav');
+    let $btn = $headerNav.find('.user-menu');
+    
+    $btn.on('click', function(e){
+        e.preventDefault();
+        let _this = $(this);
+        
+        if($headerNav.hasClass('on')){
+            _this.attr('title', 'open');
+            $headerNav
+                .removeClass('on')
+                .find('.header__nav-list').removeClass('show');
+        }else{
+            _this.attr('title', 'close');
+            $headerNav
+                .addClass('on')
+                .find('.header__nav-list').addClass('show');
+        }
+    })
+
+}
+
+const mobileMenu = function(){
+    if(!isMobile) return false;
+
+    let $headerNav = $('.header__nav');
+    const $menu = $headerNav.find('.mobile-menu');
+
+    $menu.on('click', function(e){
+        e.preventDefault();
+        
+        if($headerNav.hasClass('show')){
+            $headerNav.removeClass('show');
+            $menu.attr('title', 'open');
+            $headerNav.find('.inner').slideUp();
+        }else{
+            $headerNav.addClass('show');
+            $menu.attr('title', 'close');
+            $headerNav.find('.inner').slideDown();
+        }
+    })
+}
+
+$(document).ready(function(){
+    
+    $('.btn_modal').on('click', function(e){
+        e.preventDefault();
+        modals.open(this);
+    });
+
+    $('#wrap').on('click', '.modal_close', function(e){
+        e.preventDefault();
+        modals.close();
+    });
+
+    userMenu();
+    mobileMenu();
+});
---
+++ src/@asset/js/main.js
@@ -0,0 +1,78 @@
+
+const slider = function(){
+    let $slider = $('.visual-slide');
+    let $item = $slider.find('.visual-item');
+    
+    if($item.length > 1){
+        $slider.one('init', function(){
+            let $firstItem = $item.eq(0);
+            let bg = $firstItem.data('bg');
+
+            bg && $('.kv-visual').css('background', bg);
+        });
+        $slider.slick({ dots: true });
+        $slider.on('beforeChange', function(event, slick, curr, next){
+            let $nextItem = slick.$slides.eq(next);
+            let bg = $nextItem.data('bg');
+
+            $('.kv-visual').removeAttr('style');
+            bg && $('.kv-visual').css('background', bg);
+
+        });
+        
+    }
+};
+
+const sorting = function(){
+    let $sortList = $('.sort-list');
+    let $sortItem = $sortList.find('li');
+    let initCate = $('.sort-cate .btn-sort').eq(0).data('cate');
+    let initArray= [];
+
+    const init = function(){
+        $sortItem.each(function(){
+            let _this = $(this);
+            initArray.push(_this.data('sort'));
+        });
+
+        addEvent();
+    }
+
+    const sort = function(el){
+        let _this = $(el);
+        let cate = _this.data('cate');
+        let sortingArray = [];
+
+        $sortItem.removeAttr('style');
+        
+        if(initCate == cate) return false;
+        
+        initArray.forEach(function(str, i){
+            if(str.indexOf(cate) == -1) {
+                sortingArray.push(str) ;
+                $sortItem.eq(i).hide();
+            }
+        });
+    }
+
+    const addEvent = function(){
+        $('.btn-sort').on('click', function(e){
+            e.preventDefault();
+            sort(this);
+
+            let $parent = $(this).parent();
+            $parent.addClass('active').siblings().removeClass('active')
+        });
+    }
+
+    init();
+
+}
+
+
+$(document).ready(function(){
+    
+    slider();
+    sorting();
+
+});
